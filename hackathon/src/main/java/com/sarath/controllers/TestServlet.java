f394ed8b33409f3f58071de1955eaf794d91b937
sarath77 <krish@DESKTOP-R2RNFT9>
2017-06-07 19:18:50 +0530

    changed java files loc

--- hackathon/src/main/java/com/sarath/controllers/TestServlet.java
+++ hackathon/src/main/java/com/sarath/controllers/TestServlet.java
@@ -1,15 +1,16 @@
 package com.sarath.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.ui.ModelMap;
 
 @Controller
+@EnableWebMvc
 @RequestMapping("/hello")
 public class TestServlet {
-	@RequestMapping(method = RequestMethod.GET)
-   public String printHello(Model  model) {
+   @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
       return "hello";
    }
